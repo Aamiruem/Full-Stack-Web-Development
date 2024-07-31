fda9d02bbade44495308dd69e50b3e16a127484c
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-31 23:08:43 +0530

    commit add

---
+++ complete javaScript/BasicsPart1/Array.js
@@ -0,0 +1,55 @@
+// let marks = ["chemistry" = 90, "physics" = 97, "math" = 96, "computer" = 94];
+
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+
+// console.log(marks[4]); // undefined
+
+// console.log(marks.length);
+
+// marks[4] = 98;
+// console.log(marks);
+
+// marks[marks.length] = 99;
+// console.log(marks);
+
+// marks[marks.length] = 100;
+
+
+let marks = [90, 97, 96, 94];
+let friends = ["kamran", "suresh", "Ashok", 'sonu'];
+
+console.log(marks);
+
+console.log(friends);
+
+
+let player = {name: "Virat",
+    surName: "Kohli",
+    age: 32,
+    debut: "august 2008",
+    totalRuns: 14000,
+    country: "India"
+}
+
+console.log(player);
+
+
+
+
+let actor = {
+name: "Pankaj",
+surName: "Tripathi",
+hitSeries: "Mirzapur",
+character: "Kaleen Bhaiya",
+dialogue: "Impt ye hnin ki...",
+// II
+};
+console.log(player.name);
+console.log(player.surName);
+console.log(player.age);
+console.log(actor.name);
+console.log(actor. surName);
+console.log(actor.dialogue);
---
+++ complete javaScript/BasicsPart1/dynamically.js
@@ -0,0 +1,21 @@
+// js is a dynamically
+
+let a = 10;
+console.log(a);
+a = "Hello World";
+console.log(a);
+
+let b = [1, 2, 3];
+b.push(4);
+console.log(b);
+
+let c = { name: "John", age: 30 };
+console.log(c);
+
+c.city = "New York";
+console.log(c);
+
+var x;
+x = 5; // now x is number
+x = "john";//now x is a string 
+console.log(x);
